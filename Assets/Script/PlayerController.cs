bba19f30e79904043301c875aa4f376fdc558c43
Prabhat <prabhatakumarpatro4581@gmail.com>
2022-01-18 21:02:54 +0530

    add player flipmovement

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,36 +5,65 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-
-    bool crouch;
+    float speed;
     // private void Awake()
     // {
     //     Debug.Log("Player controller awake" );
     // }
+
     //   private void OnCollisionEnter2D(Collision2D collision){
-    //     Debug.Log("Collision:" + collision.gameObject.name);
-    //   }
+      //   Debug.Log("Collision:" + collision.gameObject.name);
+       //}
     public void Update()
     {
-        //PlayerFlip();
-        //running
-        float speed = Input.GetAxisRaw("Horizontal");//it desplay the the speed of a player = 1
-        animator.SetFloat("speed", Mathf.Abs(speed));//in it real value
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
-        Vector3 playerFlip = transform.localScale;//it is take the value from -x site it halp toward left site
-        if (speed < 0) playerFlip.x = -1f * Mathf.Abs(playerFlip.x);
-        else if (speed > 0) playerFlip.x = Mathf.Abs(playerFlip.x);
 
-        transform.localScale = playerFlip;
-
-        //jump
-       //bool jump = Input.GetAxisRaw("Vertical");
-        //Input.GetKeyDown(KeyCode.Space);
-         Input.GetAxisRaw("Vertical");
-        Input.GetKeyDown(KeyCode.Space);
+        MoveCharacter(horizontal);
+        PlayMovementAnimation(horizontal, vertical); 
     }
 
-    /*
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position  = transform.position;
+        position.x = position.x+horizontal * speed * Time.deltaTime; 
+        transform.position=position;
+    }
+
+    private void PlayMovementAnimation(float horizontal, float vertical)
+    {
+        //running
+       speed = Input.GetAxisRaw("Horizontal");//it desplay the the speed of a player = 1
+        animator.SetFloat("speed", Mathf.Abs(speed));//in it real value
+
+        Vector3 Scale = transform.localScale;//it is take the value from -x site it halp toward left site
+        //(distance/sec)*(sec/30)
+        if (speed < 0) Scale.x = -1f * Mathf.Abs(Scale.x);
+        else if (speed > 0) Scale.x = Mathf.Abs(Scale.x);
+
+        transform.localScale = Scale;
+
+
+        //jump
+        
+        if(vertical>0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+    }
+
+}
+
+
+
+
+/*
     void PlayerFlip()
     {
         float speed = Input.GetAxisRaw("Horizontal");
@@ -52,5 +81,3 @@ public class PlayerController : MonoBehaviour
     }
 
     */
-
-}
