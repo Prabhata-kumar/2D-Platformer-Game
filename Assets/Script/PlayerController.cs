a91a3400e4046a3c062b85556a1d6420c358b029
Prabhat <prabhatakumarpatro4581@gmail.com>
2022-01-20 11:59:23 +0530

    adding all the featers 3 it with a proper test

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,48 +6,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    float speed;
-    // private void Awake()
-    // {
-    //     Debug.Log("Player controller awake" );
-    // }
-
-    //   private void OnCollisionEnter2D(Collision2D collision){
-      //   Debug.Log("Collision:" + collision.gameObject.name);
-       //}
+    public float speed;
+   
     public void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Jump");
 
 
-        MoveCharacter(horizontal);
+        MoveCharacter(horizontal, vertical);
         PlayMovementAnimation(horizontal, vertical); 
     }
 
-    private void MoveCharacter(float horizontal)
-    {
+    private void MoveCharacter(float horizontal, float vertical)
+    {//move the character in horizontaly
+     
         Vector3 position  = transform.position;
-        position.x = position.x+horizontal * speed * Time.deltaTime; 
+        position.x = position.x + horizontal * speed * Time.deltaTime; 
         transform.position=position;
     }
 
     private void PlayMovementAnimation(float horizontal, float vertical)
     {
         //running
-       speed = Input.GetAxisRaw("Horizontal");//it desplay the the speed of a player = 1
-        animator.SetFloat("speed", Mathf.Abs(speed));//in it real value
+      //speed = Input.GetAxisRaw("Horizontal");//it desplay the the speed of a player = 1
+        animator.SetFloat("speed", Mathf.Abs(horizontal));//in it real value
 
         Vector3 Scale = transform.localScale;//it is take the value from -x site it halp toward left site
         //(distance/sec)*(sec/30)
-        if (speed < 0) Scale.x = -1f * Mathf.Abs(Scale.x);
-        else if (speed > 0) Scale.x = Mathf.Abs(Scale.x);
+        if (horizontal < 0) Scale.x = -1f * Mathf.Abs(Scale.x);
+        else if (horizontal > 0) Scale.x = Mathf.Abs(Scale.x);
 
         transform.localScale = Scale;
 
 
         //jump
-        
         if(vertical>0)
         {
             animator.SetBool("Jump", true);
@@ -56,6 +50,17 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Jump", false);
         }
 
+
+        //Crouch
+        bool Crouch;
+        if (vertical < 0)
+        {
+            animator.SetBool("s", true);
+        }
+        else
+        {
+            animator.SetBool("s", false);
+        }
     }
 
 }
